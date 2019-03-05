abcdc4e98e3ff6e88cbeac2fe0b6c62b27e24aeb
Pietro Monteiro <pietro@riseup.net>
2019-03-04 22:34:53 -0800

    Use TARGET_CC and install qemu to test on arm.

--- ci/dependencies.sh
+++ ci/dependencies.sh
@@ -8,17 +8,23 @@ case "$1" in
     x86_64-unknown-linux-gnu)
         ;;
     aarch64-unknown-linux-gnu)
+        export QEMU_LD_PREFIX=/usr/aarch64-linux-gnu
         apt-get -qqy --no-install-recommends install \
+                binfmt-support \
                 gcc-aarch64-linux-gnu \
-                libc6-dev-arm64-cross
+                libc6-dev-arm64-cross \
+                qemu-user-binfmt
         ;;
     i686-unknown-linux-gnu)
         apt-get -qqy --no-install-recommends install gcc-multilib
         ;;
     arm-unknown-linux-gnueabihf)
+        export QEMU_LD_PREFIX=/usr/arm-linux-gnueabihf
         apt-get -qqy --no-install-recommends install \
+                binfmt-support \
                 gcc-arm-linux-gnueabihf \
-                libc6-dev-armhf-cross
+                libc6-dev-armhf-cross \
+                qemu-user-binfmt
         ;;
     *)
         echo "UNKNOWN TARGET: $TARGET"
