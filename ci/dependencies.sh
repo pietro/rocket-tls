948e2ac548099215ebf64886583dd59bd9cd2f8c
Pietro Monteiro <pietro@riseup.net>
2019-03-04 22:04:31 -0800

    Install libc cross compilation packages.

--- ci/dependencies.sh
+++ ci/dependencies.sh
@@ -8,13 +8,17 @@ case "$1" in
     x86_64-unknown-linux-gnu)
         ;;
     aarch64-unknown-linux-gnu)
-        apt-get -qqy --no-install-recommends install gcc-aarch64-linux-gnu
+        apt-get -qqy --no-install-recommends install \
+                gcc-aarch64-linux-gnu \
+                libc6-dev-arm64-cross
         ;;
     i686-unknown-linux-gnu)
         apt-get -qqy --no-install-recommends install gcc-multilib
         ;;
     arm-unknown-linux-gnueabihf)
-        apt-get -qqy --no-install-recommends install gcc-arm-linux-gnueabihf
+        apt-get -qqy --no-install-recommends install \
+                gcc-arm-linux-gnueabihf \
+                libc6-dev-armhf-cross
         ;;
     *)
         echo "UNKNOWN TARGET: $TARGET"
