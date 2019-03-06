d0c5fe8b6e8ca1ea097aa6964291a273053af0fe
Pietro Monteiro <pietro@riseup.net>
2019-03-05 23:36:03 -0800

    Do release builds for reprotest.

--- ci/reprotest.sh
+++ ci/reprotest.sh
@@ -8,5 +8,5 @@ mkdir -p "$TMPDIR"
 reprotest -vv --variations=-all,build_path --source-pattern 'Cargo.* src/' '
     CARGO_HOME="$PWD/.cargo" RUSTUP_HOME='"$HOME/.rustup"' \
         RUSTFLAGS="--remap-path-prefix=$HOME=/remap-home --remap-path-prefix=$PWD=/remap-pwd" \
-        cargo build -vv' \
+        cargo build -v --release' \
           target/release/rocket_tls
