825bd28008382cafeb14cb063a390e1ff9141789
Pietro Monteiro <pietro@riseup.net>
2019-03-05 21:56:02 -0800

    Use debug build for reprotest.

--- ci/reprotest.sh
+++ ci/reprotest.sh
@@ -8,5 +8,5 @@ mkdir -p "$TMPDIR"
 reprotest -vv --vary=-time,-domain_host --source-pattern 'Cargo.* src/' '
     CARGO_HOME="$PWD/.cargo" RUSTUP_HOME='"$HOME/.rustup"' \
         RUSTFLAGS="--remap-path-prefix=$HOME=/remap-home --remap-path-prefix=$PWD=/remap-pwd" \
-        cargo build --release -vv' \
+        cargo build -vv' \
           target/release/rocket_tls
