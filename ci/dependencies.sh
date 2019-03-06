1b14617b95a358682ba5bfd9f2fb8ed6d085c035
Pietro Monteiro <pietro@riseup.net>
2019-03-05 23:30:31 -0800

    Install faketime for reprotest.

--- ci/dependencies.sh
+++ ci/dependencies.sh
@@ -40,7 +40,7 @@ esac
 
 case "${BUILD_MODE-}" in
     reprotest)
-        apt-get -qqy --no-install-recommends install diffoscope python3-pip python3-setuptools
+        apt-get -qqy --no-install-recommends install diffoscope faketime python3-pip python3-setuptools
         pip3 install reprotest
         ;;
     *)
