bf1713cea0dc0d8ef924783530dd31f02f60b6a3
Pietro Monteiro <pietro@riseup.net>
2019-03-05 21:31:03 -0800

    Use different tmp dir for reprotest.

--- ci/reprotest.sh
+++ ci/reprotest.sh
@@ -2,6 +2,9 @@
 
 set -eux -o pipefail
 
+export TMPDIR="$HOME/tmp/repro-test"
+mkdir -p "$TMPDIR"
+
 reprotest -vv --vary=-time,-domain_host --source-pattern 'Cargo.* src/' '
     CARGO_HOME="$PWD/.cargo" RUSTUP_HOME='"$HOME/.rustup"' \
         RUSTFLAGS="--remap-path-prefix=$HOME=/remap-home --remap-path-prefix=$PWD=/remap-pwd" \
